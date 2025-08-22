b4ea5ee9adde64f5ac93543ae5c8dcaec9680e74
= <luizhenriquegithub@gmail.com>
2025-08-22 15:04:59 -0300

    Add Request test

---
+++ Test/Request/AdministradorRequestTest.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Request
+{
+    [TestClass]
+    public class AdministradorRequestTest
+    {
+        [TestMethod]
+        public void Testar()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+    }
+}
